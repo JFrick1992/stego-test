5322c38397928d3f7d3e17f330e2789a29414eea
Jacob Frick <jacobhfrick1992@gmail.com>
2020-09-04 14:22:15 -0700

    added some crypto code

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const encode = c => {
+
+const crypto = require('crypto')
+
+const charToBinary = c => {
     let encodedChar = (c.charCodeAt()).toString(2)
     const str = new Array(8-encodedChar.length + 1).join('0')
     return str + encodedChar
 }
-const decode = bin => {
+const binaryToChar = bin => {
     return String.fromCharCode(parseInt(bin, 2))
 }
+const iv = crypto.randomBytes(16)
+
+//const buff = Buffer.from(s)
+console.log(iv)
+const secret_message = "this is a secret message"
+let key = '1234567812345678123456781234567g'
+let cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
+let encrypted = cipher.update(secret_message, 'utf-8', 'hex')
+encrypted += cipher.final('hex')
+const b64 = iv.slice(0,iv.length).toString('base64')
+console.log(b64)
+const buff = Buffer.from(b64, 'base64')
+console.log(buff)
+console.log('encrypted: ' + encrypted)
+
+let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+decrypted += decipher.final('utf-8');
+
+console.log('decrypted: ' + decrypted)

c5bce2d86fda44a268f7a36dcab1a36cb7a9170f
Jacob Frick <jacobhfrick1992@gmail.com>
2020-09-06 17:31:00 -0700

    added encrypt and decrypt functions for text messages

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
+const crypto = require("crypto")
 
-const crypto = require('crypto')
-
-const charToBinary = c => {
-    let encodedChar = (c.charCodeAt()).toString(2)
-    const str = new Array(8-encodedChar.length + 1).join('0')
+const charToBinary = (c) => {
+    let encodedChar = c.charCodeAt().toString(2);
+    const str = new Array(8 - encodedChar.length + 1).join("0")
     return str + encodedChar
 }
-const binaryToChar = bin => {
+const binaryToChar = (bin) => {
     return String.fromCharCode(parseInt(bin, 2))
 }
 
-const getIv = _ => crypto.randomBytes(16)
-const getIvFromBase64 = b64 => Buffer.from(b64, 'base64')
-const getRandomKey = _ => crypto.randomBytes(32).toString('base64')
-const getBase64 = iv => iv.toString('base64')
+const get16ByteIv = (_) => crypto.randomBytes(16)
+const getIvFromBase64 = (b64) => Buffer.from(b64, "base64")
+const getRandom32ByteKey = (_) => crypto.randomBytes(24).toString('base64')
+const getBase64 = (buffer) => buffer.toString("base64")
 
-
-// //const buff = Buffer.from(s)
-// console.log(iv)
-// const secret_message = "this is a secret message"
-// let key = '1234567812345678123456781234567g'
-// let cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
-// let encrypted = cipher.update(secret_message, 'utf-8', 'hex')
-// encrypted += cipher.final('hex')
-// const b64 = iv.slice(0,iv.length).toString('base64')
-// console.log(b64)
-// const buff = Buffer.from(b64, 'base64')
-// console.log(buff)
-// console.log('encrypted: ' + encrypted)
-
-// let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-// let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
-// decrypted += decipher.final('utf-8');
-
-// console.log('decrypted: ' + decrypted)
+const encrypt = (message, key, iv) => {
+    const cipher = crypto.createCipheriv("aes-256-cbc", key, iv)
+    let encrypted = cipher.update(message, "utf-8", "hex")
+    encrypted += cipher.final("hex")
+    encrypted = `${getBase64(iv)}.${encrypted}`
+    return encrypted
+}
+const decrypt = (encryptedMessage, key) => {
+    const [iv, message] = encryptedMessage.split('.')
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, getIvFromBase64(iv))
+    let decrypted = decipher.update(message, 'hex', 'utf-8')
+    decrypted += decipher.final('utf-8')
+    return decrypted
+}
+//Example
+// const key = getRandom32ByteKey()
+// const iv = get16ByteIv()
+// const message = 'Hello beautiful'
+// const encryptedMessage = encrypt(message, key, iv)
+// console.log(encryptedMessage)
+// console.log(decrypt(encryptedMessage, key))

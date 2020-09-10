7344426e0dfa3206c0bdab0d2bd29310e82fcf80
Jacob Frick <jacobhfrick1992@gmail.com>
2020-09-09 23:30:40 -0700

    added comments + code refactor

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
+//requires
 const crypto = require("crypto")
 const Jimp = require('jimp')
+
+//Converts a single character to an 8-bit binary code
 const charToBinary = (c) => {
     let encodedChar = c.charCodeAt().toString(2)
-    const str = new Array(8 - encodedChar.length + 1).join("0")
-    return str + encodedChar
+    return new Array(8 - encodedChar.length + 1).join("0") + encodedChar
 }
-const binaryToChar = (bin) => {
-    return String.fromCharCode(parseInt(bin, 2))
-}
+
+//Converts 8-bit binary code to a single character
+const binaryToChar = (bin) => String.fromCharCode(parseInt(bin, 2))
+
+//Converts a binary string, thats length%8===0, to a string  
 const convertBinaryToString = bin => {
     let str = ''
-    for(let i = 0; i < bin.length; i+=8) {
+    for (let i = 0; i < bin.length; i += 8) {
         str += binaryToChar(bin.substring(i, i + 8))
     }
     return str
 }
+//Loops over a string and converts it to binary, and returns binary string
 const convertStringToBinary = str => {
     let binaryString = ''
-    for(let c in str) {
+    for (let c in str) {
         binaryString += charToBinary(str[c])
     }
     return binaryString
 }
+
+//Gets a random 16 Byte IV for encryption
 const get16ByteIv = (_) => crypto.randomBytes(16)
+
+//Converts a base64 string to an IV, used to reverse IV stored on encrypted message
 const getIvFromBase64 = (b64) => Buffer.from(b64, "base64")
+
+//Returns a random base64 string thats 32 bytes long, alternate to using hashed password
 const getRandom32ByteKey = (_) => crypto.randomBytes(24).toString('base64')
+
+//Converts buffer to base64 string
 const getBase64 = (buffer) => buffer.toString("base64")
 
+//Takes in message, a key, and iv (key and iv should be as random as possible) and uses aes-256-cbc encryption algorithm to encrypt message
 const encrypt = (message, key, iv) => {
     const cipher = crypto.createCipheriv("aes-256-cbc", key, iv)
     let encrypted = cipher.update(message, "utf-8", "hex")
@@ -34,6 +48,8 @@ const encrypt = (message, key, iv) => {
     encrypted = `${getBase64(iv)}.${encrypted}`
     return encrypted
 }
+
+//Takes in the encrypted message and the key, and decrypts the message, using aes-256-cbc encryption algorithm
 const decrypt = (encryptedMessage, key) => {
     const [iv, message] = encryptedMessage.split('.')
     let decipher = crypto.createDecipheriv('aes-256-cbc', key, getIvFromBase64(iv))
@@ -42,17 +58,24 @@ const decrypt = (encryptedMessage, key) => {
     return decrypted
 }
 
+//Gets length of string and converts to binary string 32-bits in length
 const getStringLenInBinary = str => new Array(32 - str.length.toString(2).length + 1).join("0") + str.length.toString(2)
+
+
 const getImage = (imagePath, cb) => {
     Jimp.read(imagePath, (err, image) => {
         if (err) throw err
         else return cb(image)
     })
 }
+const encodeMessageLength = (buffer, length) => {
+    //length will be 32 bits long
+}
 const LSBDataToImage = (message) => {
 
 
 }
+
 
 
 
